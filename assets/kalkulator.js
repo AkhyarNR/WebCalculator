884e05b4f9cc5595e6b7d2e20ff0c8545507b2ea
Imyarrr <akhyar.arspada09@gmail.com>
2021-06-11 15:58:54 +0700

    Checkout

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,5 +1,3 @@
-console.log('asdfasfdasd');
-document.querySelector("#displayNumber").innerHTML = '<em>Tiga Anak Kucing</em>';
 const calculator = {
     displayNumber: '0',
     operator: null,
@@ -19,7 +17,11 @@ function clearCalculator(){
 }
 
 function inputDigit(digit){
-    calculator.displayNumber += digit;
+    if(calculator.displayNumber === '0'){
+        calculator.displayNumber = digit;
+    }else{
+        calculator.displayNumber += digit;
+    }
 }
 
 const buttons = document.querySelectorAll(".button");
@@ -29,7 +31,78 @@ for(let button of buttons){
         // mendapatkan object elemen yang diklik
         const target = event.target;
 
+        if(target.classList.contains('clear')){
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('negative')){
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('equals')){
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('operator')){
+            handleOperator(target.innerText);
+            return;
+        }
+
         inputDigit(target.innerText);
         updateDisplay()
     });
+}
+
+function inverseNumber(){
+    if(calculator.displayNumber === '0'){
+        return;
+    }
+
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator){
+    if(!calculator.waitingForSecondNumber){
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        //mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+        calculator.displayNumber = '0';
+    }else{
+        alert('Operator sudah ditetapkan')
+    }
+}
+
+function performCalculation(){
+    if(calculator.firstNumber == null || calculator.operator == null){
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if(calculator.operator === "+"){
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    }else{
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+
+    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+
+    putHistory(history);
+    calculator.displayNumber = result;
+    renderHistory();
 }
